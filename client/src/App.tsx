b84b893495261b919408db87929176ce1b4deabd
Taras Khmelovskyi <khmelovski@gmail.com>
2021-03-14 13:33:42 +0200

    feat: move client to folder

---
+++ client/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Button from '@material-ui/core/Button'
+function App() {
+	return (
+		<div className="App">
+			<h1>React Game</h1>
+			<Button variant="contained" color="primary">
+				Hello World
+			</Button>
+		</div>
+	)
+}
+
+export default App
