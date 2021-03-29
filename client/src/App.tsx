ddcc1f2c7d1f44bf4f73289883aa6d7e02d69bca
tkhmelovskyi <taras.khmelovskyi@eleks.com>
2021-03-29 23:36:05 +0300

    feat: add simple GUARD

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,17 +3,22 @@ import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 
 import AuthLayout from './containers/AuthLayout'
 import SiteLayout from './containers/SiteLayout'
+import { Cards } from './components/Cards'
+import PrivateRoute from './helpers/GuardedRoute'
 function App() {
 	return (
 		<Router>
 			<Switch>
-
 				<Route path="/auth">
 					<AuthLayout />
 				</Route>
 				<Route exact path="/">
 					<SiteLayout />
 				</Route>
+				<PrivateRoute
+					path='/list'
+					component={Cards}
+				></PrivateRoute>
 			</Switch>
 		</Router>
 	)
